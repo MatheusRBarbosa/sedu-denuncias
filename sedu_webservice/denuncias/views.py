4517156d730536a922ad1ce9760061e89e9037c4
matheus.barbosa <matheuskleber@live.com>
2019-11-28 19:03:14 -0300

    feat(filter): Aplicando filtros dropbox no resultado da lista

--- sedu_webservice/denuncias/views.py
+++ sedu_webservice/denuncias/views.py
@@ -8,12 +8,14 @@ from django.utils.decorators import method_decorator
 # specific to this view
 from django.views.generic import ListView, DetailView 
 from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
+from django import forms
 
 ##Paginas Web
 @method_decorator(login_required, name='dispatch')
 class ReclamacaoList(ListView):
     #model = Reclamacao
     context_object_name = 'reclamacoes'
+    #form_class = ReclamacaoListForm
     queryset = Reclamacao.objects.all()
 
     def get_context_data(self, **kwargs):
@@ -22,6 +24,20 @@ class ReclamacaoList(ListView):
         context['SREs'] = SRE.objects.all()
         context['tipos'] = TipoReclamacao.objects.all()
         return context
+    
+    def load_reclamacoes(request):
+        tipo_id = request.GET.get('tipo')
+        sre_id = request.GET.get('sre')
+        
+        if(tipo_id != '0' and sre_id == '0'):
+            reclamacoes = Reclamacao.objects.filter(tipo=tipo_id)
+        elif(tipo_id == '0' and sre_id != '0'):
+            reclamacoes = Reclamacao.objects.filter(aluno__escola__municipio__sre__id=sre_id)
+        elif(tipo_id != '0' and sre_id != '0'):
+            reclamacoes = Reclamacao.objects.filter(aluno__escola__municipio__sre__id=sre_id, tipo=tipo_id)
+        else:
+            reclamacoes = Reclamacao.objects.all()
+        return render(request, 'denuncias/filtered/reclamacao_list_filtered.html', {'reclamacoes': reclamacoes })
 
 @method_decorator(login_required, name='dispatch')
 class ReclamacaoDetail(DetailView): 
