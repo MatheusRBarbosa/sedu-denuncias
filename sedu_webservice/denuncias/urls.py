092b64d280c550c83342917f30f8834d674d0ca9
Matheus Barbosa <matheuskleber@live.com>
2020-01-30 08:41:46 -0200

    chore(aluno): removendo create de aluno

--- sedu_webservice/denuncias/urls.py
+++ sedu_webservice/denuncias/urls.py
@@ -9,7 +9,6 @@ urlpatterns = [
     path('reclamacao/<int:pk>/comentario_create', ComentarioCreate.as_view(), name='web_comentario_create'),
     path('reclamacao/<int:pk>/parecer_final_create', ParecerFinalCreate.as_view(), name='web_parecer_final'),
     path('reclamacao/<int:pk>/encaminhar', Encaminhar.as_view(), name='web_encaminhar_create'),
-    path('aluno_create', AlunoCreate.as_view(), name='web_aluno_create'),
     path('reclamante_create', ReclamanteCreate.as_view(), name='web_reclamante_create'),
     path('ajax/load-rotas', load_rotas, name='ajax_load_rotas'),
     path('ajax/load-municipios', load_municipios, name='ajax_load_municipios'),
