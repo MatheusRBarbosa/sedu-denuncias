ef30f5e596bff1c89a6c245553bda9605b0289d7
Matheus Barbosa <matheuskleber@live.com>
2020-02-14 18:47:48 -0300

    feat(api): Removendo endpoints

--- sedu_webservice/denuncias/views.py
+++ sedu_webservice/denuncias/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from .models import *
-from .serializers import *
 from rest_framework import viewsets
 from django.urls import reverse_lazy
 from django.contrib.auth.decorators import login_required
