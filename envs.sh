3d9e1f7e1e9afb9b588dabb0efba48ba15a4e150
Matheus Barbosa <matheuskleber@live.com>
2020-02-20 08:55:36 -0300

    chore(envssh): alterando envs.sh

--- envs.sh
+++ envs.sh
@@ -1,4 +1,4 @@
-export DB_NAME=sedu
-export DB_USERNAME=sedu
-export DB_PASSWORD=Pg4V~u495~4x
-export DB_HOST=den1.mysql4.gear.host
+export DB_NAME=seduserver
+export DB_USERNAME=seduserver
+export DB_PASSWORD='Yd4?!iYbG4Sx'
+export DB_HOST=den1.mssql8.gear.host
